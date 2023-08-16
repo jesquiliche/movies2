1b774b871be4108123c6b866ff3737d6780b82bf
Jesús Quintana <jesquiliche@hotmail.comm>
2023-08-16 15:38:26 +0200

    Animacion

--- src/components/animacion.jsx
+++ src/components/animacion.jsx
@@ -8,7 +8,7 @@ const Animacion = ({children}) => {
     initial={{ opacity: 0.5, scale: 0.3}}
     animate={{ opacity: 1, scale: 1 }}
     exit={{ opacity: 0,scale: 0.2 }}
-    transition={{ duration: 1 }}
+    transition={{ duration: 2 }}
   >
       {children}
     </motion.div>
