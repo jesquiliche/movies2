31a5626a731c3abce2ebdb6048b50e51f6f4c646
Jesús Quintana <jesquiliche@hotmail.comm>
2023-08-16 15:28:18 +0200

    Animacion

--- src/components/ListaPaginada.jsx
+++ src/components/ListaPaginada.jsx
@@ -6,7 +6,7 @@ import Animacion from "./animacion";
 
 const ListaPaginada = ({ data, indice }) => {
   return (
-    <Animacion>
+    <>
       <div
         className={`grid grid-cols-1 sm:grid-cols-${indice} md:grid-cols-5 lg:grid-cols-${indice} xl:grid-cols-${indice} gap-4 mx-2 `}
       >
@@ -50,7 +50,7 @@ const ListaPaginada = ({ data, indice }) => {
           </div>
         ))}
       </div>
-    </Animacion>
+    </>
   );
 };
 
