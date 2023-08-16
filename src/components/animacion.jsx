b6d7e1ec1c4435a493f4429a5ff8a22743267acc
Jesús Quintana <jesquiliche@hotmail.comm>
2023-08-16 15:44:15 +0200

    Animacion

--- src/components/animacion.jsx
+++ src/components/animacion.jsx
@@ -5,10 +5,10 @@ const Animacion = ({children}) => {
   return (
     <motion.div
     className="items-center"
-    initial={{ opacity: 0.5, scale: 0.5}}
+    initial={{ opacity: 0.5, scale: 0.7}}
     animate={{ opacity: 1, scale: 1 }}
     exit={{ opacity: 0,scale: 0.2 }}
-    transition={{ duration: 2 }}
+    transition={{ duration: 1.5 }}
   >
       {children}
     </motion.div>
